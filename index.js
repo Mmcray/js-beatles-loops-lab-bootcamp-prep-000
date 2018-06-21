cebc02473360d1a45113b6de706fce5480ad26b4
Michael McRay <mmcray5@gmail.com>
2018-06-21 23:32:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function theBeatlesPlay(musicians, instruments) {
+  var beatles = [];
+  var i;
+  var mus = musicians.length;
+  for (i = 0; i < mus; i++) {
+  beatles.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return beatles;
+}
+function johnLennonFacts(array) {
+while (i < array.length) {
+  
+}
